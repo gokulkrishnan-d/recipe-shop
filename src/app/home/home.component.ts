589c9ad9e5284fb417ed37f3ee051ac9a8eff298
Gokulkrishnan <gokulkrishnan.d@tcs.com>
2020-11-06 21:26:43 +0530

    Custom Observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ɵConsole } from '@angular/core';
+import {interval, Subscription, Observable} from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
+  private firstObs: Subscription;
+
   ngOnInit() {
+    // this.firstObs = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // });
+
+    const cusObservable = new Observable((observer) => {
+      let count = 0;
+      setInterval(() => {
+          if (count > 10) {
+            observer.error(new Error('Count i greater that 10'));
+          }
+          if (count === 2){
+            observer.complete();
+          }
+          observer.next(count);
+          count++;
+      }, 1000);
+    });
+
+    this.firstObs =   cusObservable.subscribe(countVal => {
+      console.log(countVal);
+    }, (error) => {
+      console.error(error.message);
+    }, () => {
+      console.log('completed!');
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.firstObs.unsubscribe();
+
   }
 
 }
